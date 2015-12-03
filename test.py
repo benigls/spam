79fd1add9ea4428c1b8045e8e54729872e9b14f4
Ben <iglesia.ben@gmail.com>
2015-12-03 16:37:53 +0000

    Add test dataset subdirs exist and add global params.

--- test.py
+++ test.py
@@ -4,14 +4,16 @@
 import unittest
 import os
 
+import params
+
 
 class TestDataSet(unittest.TestCase):
     """
     Class for testing the enron dataset.
     """
     def setUp(self):
-        self.dataset_path = 'enron_dataset'
-        self.dataset_subdirs = ['enron', ]
+        self.dataset_path = params.DATASET_PATH
+        self.dataset_subdirs = params.DATASET_SUBDIRS
 
     def tearDown(self):
         self.dataset_dir = ''
@@ -27,6 +29,15 @@ class TestDataSet(unittest.TestCase):
         Test if the dataset path is a directory.
         """
         return self.assertTrue(os.path.isdir(self.dataset_path))
+
+    def test_dataset_subdirs_exist(self):
+        """
+        Test if the dataset sub-directories exist.
+        """
+        subdir_path = ''
+        for subdir in self.dataset_subdirs:
+            subdir_path = os.path.join(self.dataset_path, subdir)
+            self.assertTrue(os.path.exists(subdir_path))
 
 
 if __name__ == '__main__':
