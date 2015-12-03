39c51a1eb7c5073997942bd6fde7cfbf5a79945b
Ben <iglesia.ben@gmail.com>
2015-12-03 16:35:49 +0000

    Add global parameters.

---
+++ params.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+DATASET_PATH = 'enron_dataset'
+DATASET_SUBDIRS = [
+    'enron1',
+    'enron2',
+    'enron3',
+    'enron4',
+    'enron5',
+    'enron6',
+]
