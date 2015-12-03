7f594b707a8bc626fecc832d4cf842d30446e836
Ben <iglesia.ben@gmail.com>
2015-12-03 14:55:10 +0000

    Add test.

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class TestDataSet(unittest.TestCase):
+    """
+    Class for testing the enron dataset.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
