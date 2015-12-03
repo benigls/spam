0a030060a4e56633f7625156b7e97d145c47b2b1
Ben <iglesia.ben@gmail.com>
2015-12-03 16:20:36 +0000

    Update test cases.

--- test.py
+++ test.py
@@ -10,16 +10,23 @@ class TestDataSet(unittest.TestCase):
     Class for testing the enron dataset.
     """
     def setUp(self):
-        self.dataset_dir = 'enron_dataset'
+        self.dataset_path = 'enron_dataset'
+        self.dataset_subdirs = ['enron', ]
 
     def tearDown(self):
         self.dataset_dir = ''
 
-    def test_dataset_folder_exist(self):
-        return self.assertEqual(True, os.path.exists(self.dataset_dir))
+    def test_dataset_path_exist(self):
+        """
+        Test if the dataset path exist.
+        """
+        return self.assertTrue(os.path.exists(self.dataset_path))
 
-    def test_dataset_is_a_folder(self):
-        return self.assertEqual(True, os.path.isdir(self.dataset_dir))
+    def test_dataset_path_is_folder(self):
+        """
+        Test if the dataset path is a directory.
+        """
+        return self.assertTrue(os.path.isdir(self.dataset_path))
 
 
 if __name__ == '__main__':
