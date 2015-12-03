60720a9f34b47d8740e8a51ace54ed76c67d1311
Ben <iglesia.ben@gmail.com>
2015-12-03 17:24:17 +0000

    Add test for spam and ham folders.

--- test.py
+++ test.py
@@ -52,6 +52,33 @@ class TestDataSet(unittest.TestCase):
         for subdir in self.dataset_subdirs_path:
             self.assertTrue(os.path.isdir(subdir))
 
+    def test_dataset_spam_folder_exist(self):
+        """
+        Test if the dataset sub-directories has a spam folder.
+        """
+        for subdir in self.dataset_subdirs_path:
+            self.assertTrue(os.path.exists(os.path.join(subdir, 'spam')))
+
+    def test_dataset_ham_folder_exist(self):
+        """
+        Test if the dataset sub-directories has a ham folder.
+        """
+        for subdir in self.dataset_subdirs_path:
+            self.assertTrue(os.path.exists(os.path.join(subdir, 'ham')))
+
+    def test_dataset_spam_is_folder(self):
+        """
+        Test if the dataset sub-directories spam folder is a folder.
+        """
+        for subdir in self.dataset_subdirs_path:
+            self.assertTrue(os.path.isdir(os.path.join(subdir, 'spam')))
+
+    def test_dataset_ham_is_folder(self):
+        """
+        Test if the dataset sub-directories ham folder is a folder.
+        """
+        for subdir in self.dataset_subdirs_path:
+            self.assertTrue(os.path.isdir(os.path.join(subdir, 'ham')))
 
 if __name__ == '__main__':
     unittest.main()
