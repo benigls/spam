a1fa3e09ce0d5cb5bbf0e899d4a694b07fe2cbc7
Ben <iglesia.ben@gmail.com>
2015-12-04 06:22:31 +0000

    Update params.

--- params.py
+++ params.py
@@ -3,10 +3,40 @@
 
 DATASET_PATH = 'enron_dataset'
 DATASET_SUBDIRS = [
-    'enron1',
-    'enron2',
-    'enron3',
-    'enron4',
-    'enron5',
-    'enron6',
+    {
+        'name': 'enron1',
+        'total_count': 5172,
+        'ham_count': 3672,
+        'spam_count': 1500,
+    },
+    {
+        'name': 'enron2',
+        'total_count': 5857,
+        'ham_count': 4361,
+        'spam_count': 1496,
+    },
+    {
+        'name': 'enron3',
+        'total_count': 5512,
+        'ham_count': 4012,
+        'spam_count': 1500,
+    },
+    {
+        'name': 'enron4',
+        'total_count': 6000,
+        'ham_count': 1500,
+        'spam_count': 4500,
+    },
+    {
+        'name': 'enron5',
+        'total_count': 5175,
+        'ham_count': 1500,
+        'spam_count': 3675,
+    },
+    {
+        'name': 'enron6',
+        'total_count': 6000,
+        'ham_count': 1500,
+        'spam_count': 4500,
+    },
 ]
