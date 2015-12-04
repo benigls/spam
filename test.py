4684b19bfac63ac1793e430fe8033d255dce9e55
Ben <iglesia.ben@gmail.com>
2015-12-04 06:22:35 +0000

    Update tests.

--- test.py
+++ test.py
@@ -15,16 +15,14 @@ class TestDataSet(unittest.TestCase):
         self.dataset_path = params.DATASET_PATH
         self.dataset_subdirs = params.DATASET_SUBDIRS
 
-        self.dataset_subdirs_path = []
-        for subdir in self.dataset_subdirs:
-            self.dataset_subdirs_path.append(
-                os.path.join(self.dataset_path, subdir)
+        for i in range(len(self.dataset_subdirs)):
+            self.dataset_subdirs[i]['path'] = os.path.join(
+                self.dataset_path, self.dataset_subdirs[i]['name']
             )
 
     def tearDown(self):
         self.dataset_path = ''
-        self.dataset_subdirs = ''
-        self.dataset_subdirs_path = []
+        self.dataset_subdirs = []
 
     def test_dataset_path_exist(self):
         """
@@ -42,43 +40,51 @@ class TestDataSet(unittest.TestCase):
         """
         Test if the dataset sub-directories exist.
         """
-        for subdir in self.dataset_subdirs_path:
-            self.assertTrue(os.path.exists(subdir))
+        for subdir in self.dataset_subdirs:
+            self.assertTrue(os.path.exists(subdir['path']))
 
     def test_dataset_subdirs_is_folder(self):
         """
         Test if the dataset sub-directories is a folder.
         """
-        for subdir in self.dataset_subdirs_path:
-            self.assertTrue(os.path.isdir(subdir))
+        for subdir in self.dataset_subdirs:
+            self.assertTrue(os.path.isdir(subdir['path']))
 
     def test_dataset_spam_folder_exist(self):
         """
         Test if the dataset sub-directories has a spam folder.
         """
-        for subdir in self.dataset_subdirs_path:
-            self.assertTrue(os.path.exists(os.path.join(subdir, 'spam')))
+        for subdir in self.dataset_subdirs:
+            self.assertTrue(os.path.exists(
+                os.path.join(subdir['path'], 'spam')
+            ))
 
     def test_dataset_ham_folder_exist(self):
         """
         Test if the dataset sub-directories has a ham folder.
         """
-        for subdir in self.dataset_subdirs_path:
-            self.assertTrue(os.path.exists(os.path.join(subdir, 'ham')))
+        for subdir in self.dataset_subdirs:
+            self.assertTrue(os.path.exists(
+                os.path.join(subdir['path'], 'ham')
+            ))
 
     def test_dataset_spam_is_folder(self):
         """
         Test if the dataset sub-directories spam folder is a folder.
         """
-        for subdir in self.dataset_subdirs_path:
-            self.assertTrue(os.path.isdir(os.path.join(subdir, 'spam')))
+        for subdir in self.dataset_subdirs:
+            self.assertTrue(os.path.isdir(
+                os.path.join(subdir['path'], 'spam')
+            ))
 
     def test_dataset_ham_is_folder(self):
         """
         Test if the dataset sub-directories ham folder is a folder.
         """
-        for subdir in self.dataset_subdirs_path:
-            self.assertTrue(os.path.isdir(os.path.join(subdir, 'ham')))
+        for subdir in self.dataset_subdirs:
+            self.assertTrue(os.path.isdir(
+                os.path.join(subdir['path'], 'ham')
+            ))
 
 if __name__ == '__main__':
     unittest.main()
