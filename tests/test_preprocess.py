d89ae3c62b29d18a5729df9cdad7b4d1b211a443
Ben <iglesia.ben@gmail.com>
2015-12-06 06:42:55 +0000

    Add test for instance.

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -3,13 +3,21 @@
 
 import unittest
 
+from preprocess import PreProcess
+
 
 class TestPreProcess(unittest.TestCase):
     """
     Class for testing the preprocces.
     """
     def setUp(self):
-        pass
+        self.preprocess = PreProcess()
 
     def tearDown(self):
         pass
+
+    def test_preprocess_instance(self):
+        """
+        Test if preprocess is creating a instance.
+        """
+        self.assertIsInstance(self.preprocess, PreProcess)
