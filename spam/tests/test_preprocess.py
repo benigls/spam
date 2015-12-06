7fab2f02ddea20a790c4e6065b38229776c6b763
Ben <iglesia.ben@gmail.com>
2015-12-06 16:13:03 +0000

    Add empty tests with descriptions.

--- spam/tests/test_preprocess.py
+++ spam/tests/test_preprocess.py
@@ -25,3 +25,48 @@ class TestPreProcess(unittest.TestCase):
         Test if preprocess is creating a instance.
         """
         self.assertIsInstance(self.preprocess, PreProcess)
+
+    def test_preprocess_open_email(self):
+        """
+        Test if preprocess can open email from the dataset.
+        """
+        pass
+
+    def test_preprocess_read_email(self):
+        """
+        Test if preprocess can read email from the dataset.
+        """
+        pass
+
+    def test_preprocess_regex_email(self):
+        """
+        Test if preprocess regex can remove non-alphanumeric
+        characters and the word `Subject:` and replace it with a space.
+        """
+        pass
+
+    def test_preprocess_tokenize_email(self):
+        """
+        Test if preprocess can tokenize email.
+        """
+        pass
+
+    def test_preprocess_stopwords(self):
+        """
+        Test if preprocess can remove stopwords.
+        """
+        pass
+
+    def test_preprocess_clean_email(self):
+        """
+        Test of preprocess can clean a email.
+        This involves replacing characters via regex,
+        tokenizing, and removing stopwords.
+        """
+        pass
+
+    def test_preprocess_bag_of_words(self):
+        """
+        Test if preprocess can produces a correct bag-of-words.
+        """
+        pass
