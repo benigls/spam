20ca853e276dff779c1a4db0e2fedd88b07658f5
Ben <iglesia.ben@gmail.com>
2015-12-07 09:13:03 +0000

    Add enron2 emails.

--- spam/tests/fake_params.py
+++ spam/tests/fake_params.py
@@ -111,18 +111,6 @@ DATASET_SUBDIRS = [
             very valuable to you if all your time is required to
             feed it'
             """,
-            # 0037.2003-12-22.GP.spam.txt
-            """
-            Subject: re [ 8 ] : dear friend -
-            size = 1 > order confirmation . your order should be
-            shipped by january , via fedex .
-            your federal express tracking number is 45954036 .
-            thank you for registering . your userid is : 56075519
-            learn to make a fortune with ebay !
-            complete turnkey system software - videos - turorials
-            clk here for information
-            clilings .
-            """,
         ],
     },
     {
@@ -142,19 +130,112 @@ DATASET_SUBDIRS = [
             'spam'
         ),
         'ham_emails': [
+            # 0028.2001-02-15.kitchen.ham.txt
             """
+            Subject: elektro short position
+            for this morning call thanks
+            - - - - - - - - - - - - - - - - - - - - - - forwarded by
+            orlando gonzalez / sa / enron on 15 / 02 / 2001 12 : 14 -
+            - - - - - - - - - - - - - - - - - - - - - - - - - -
+            luis henriques
+            15 / 02 / 2001 12 : 06
+            to : orlando gonzalez / sa / enron @ enron
+            cc :
+            subject : elektro short position
+            see attached a file for our today conference call .
+            luiz otavio
             """,
+            # 0042.2001-02-20.kitchen.ham.txt
             """
-            """,
-            """
+            Subject: draft press release : kcs energy
+            this press release for kcs energy has a reference to
+            ena . call me if you have any problems with the
+            attached . it is scheduled to go out today .
+            eric
+            - press release 2 - 20 - 01 final . doc
             """,
         ],
         'spam_emails': [
+            # 0037.2003-12-22.GP.spam.txt
             """
+            Subject: re [ 8 ] : dear friend -
+            size = 1 > order confirmation . your order should be
+            shipped by january , via fedex .
+            your federal express tracking number is 45954036 .
+            thank you for registering . your userid is : 56075519
+            learn to make a fortune with ebay !
+            complete turnkey system software - videos - turorials
+            clk here for information
+            clilings .
             """,
+            # 0107.2004-08-11.BG.spam.txt
             """
+            Subject: make $ 314
+            hello ,
+            we sent you an email a while ago , because you now
+            qualify for a new mortgage .
+            you could get $ 300 , 000 for as little as $ 700 a
+            month !
+            bad credit is no problem , you can pull cash out or
+            refinance .
+            please click on this link for free consultation by a
+            mortgage broker :
+            http : / / www . hgkkdc . com /
+            best regards ,
+            jamie higgs
+            no thanks : http : / / www . hgkkdc . com / rl
+            - - - - system information - - - -
+            form from area works numbering another contribution zone
+            languages ) rfc 3066 languages essential require
+            presentation submitted cannot
+        stroke - radical here collating resources intermediaries
+        identified presents
+        index
             """,
+            # 0131.2004-08-12.BG.spam.txt
             """
+            Subject: the p : ri , ce is r ; ight
+            look at the crazy prices of these high - end software packages !
+            why buy them for hundreds of dollars at the store ?
+            try them out here , they ' re 100 % money back guarantee .
+            windows xp professional 2002
+            . . . . . . . . . . . . . $ 50
+            adobe photoshop 7 . 0
+            . . . . . . . . . . . . . . . . . . . . . $ 60
+            microsoft office xp professional 2002
+            . . . $ 60
+            corel draw graphics suite 11
+            . . . . . . . . . . . . $ 60
+            and lots more . . .'
+            """,
+            # 0044.2004-08-06.BG.spam.txt
+            """
+            Subject: give your partner more pleasure
+            my girlfriend loves the results , but she doesn ' t know
+            what i do . she
+            thinks
+            it ' s natural - thomas , ca
+            i ' ve been using your product for 4 months now . i ' ve
+            increased my
+            length from 2
+            to nearly 6 . your product has saved my sex life .
+            - matt , fl
+            pleasure your partner every time with a bigger , longer ,
+            stronger unit
+            realistic gains quickly
+            to be a stud
+            press here
+            and now i must go , he continued , for my stay in your
+            city will be a
+            short one and i want to see all i can
+            oranjestad ,
+            aruba , po b 1200
+            come , ozma , she said , anxiously ; let us go ourselves
+            to search for the
+            piglet the president scrawled something on a sheet of
+            paper and signed his
+            name to it , afterward presenting it , with a courteous
+            bow , to his visitor
             """,
         ],
     },
