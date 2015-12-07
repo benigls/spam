a03a1ca1efc880470b8a09414147141c86504e43
Ben <iglesia.ben@gmail.com>
2015-12-07 09:03:09 +0000

    Add enron1 spam emails.

--- spam/tests/fake_params.py
+++ spam/tests/fake_params.py
@@ -83,11 +83,45 @@ DATASET_SUBDIRS = [
             """,
         ],
         'spam_emails': [
+            # 0011.2003-12-18.GP.spam.txt
             """
+            Subject: sup . er cha . rge your m . an hood today jvbe kfbtyra xes
+            hello ,
+            generic and super viagra ( cialis ) available online !
+            most trusted online source !
+            cialis or ( super viag )
+            takes affect right away & lasts 24 - 36 hours !
+            for super viagra click here
+            generic viagra
+            costs 60 % less ! save a lot of money .
+            for viagra click here
+            both products shipped discretely to your door
+            not interested ?
+            dycmpf
+            s uuz
+            biwven
             """,
+            # 0030.2003-12-21.GP.spam.txt
             """
+            Subject: medical info :
+            health watch for 2003
+            four seconds until picture is downloaded
+            if you hadn ' t happened to find the piglet , eureka
+            would surely have been executedyour body can not be
+            very valuable to you if all your time is required to
+            feed it'
             """,
+            # 0037.2003-12-22.GP.spam.txt
             """
+            Subject: re [ 8 ] : dear friend -
+            size = 1 > order confirmation . your order should be
+            shipped by january , via fedex .
+            your federal express tracking number is 45954036 .
+            thank you for registering . your userid is : 56075519
+            learn to make a fortune with ebay !
+            complete turnkey system software - videos - turorials
+            clk here for information
+            clilings .
             """,
         ],
     },
