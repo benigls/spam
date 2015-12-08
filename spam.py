ef4da4f081c083d88297795d145529c543d2595e
Ben <iglesia.ben@gmail.com>
2015-12-08 10:43:16 +0000

    Set random state to 0, add comments and remove print.

--- spam.py
+++ spam.py
@@ -9,16 +9,16 @@ from spam.common.utils import get_file_path_list
 
 
 file_path_list = get_file_path_list(DATASET_META)
+
+# transform list of tuple into two list
+# e.g. [('/path/to/file', 'spam')] ==> ['path/to/file'], ['spam']
 path, classification = zip(*file_path_list)
 
+# split the data into unlabeled labeled
 unlabeled_path, labeled_path, \
     unlabeled_class, labeled_class = train_test_split(
         path,
         classification,
         test_size=0.1,
+        random_state=0,
     )
-
-print(len(unlabeled_path))
-print(len(unlabeled_class))
-print(len(labeled_path))
-print(len(labeled_class))
