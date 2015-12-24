ea7746cb337b7ea384da249ec3b471c7c9a365ce
Ben <iglesia.ben@gmail.com>
2015-12-24 07:53:31 +0000

    Add spit data function to utils.

--- spam.py
+++ spam.py
@@ -3,74 +3,54 @@
 
 import numpy as np
 import pandas as pd
-# from sklearn.cross_validation import train_test_split
 
-# from spam.common import DATASET_META
-# from spam.common.utils import get_file_path_list
+from spam.common import DATASET_META
+from spam.common.utils import get_file_path_list, split_dataset
 from spam.preprocess import preprocess
 
 
-# UNLABEL = -1
-# HAM = 0
-# SPAM = 1
+UNLABEL = -1
+HAM = 0
+SPAM = 1
 
-# file_path_list = get_file_path_list(DATASET_META)
+file_path_list = get_file_path_list(DATASET_META)
 
-# # transform list of tuple into two list
-# # e.g. [('/path/to/file', 'spam')] ==> ['path/to/file'], ['spam']
-# path, classification = zip(*file_path_list)
+print('Spliting the dataset..')
+unlabeled_path, (train_path, train_class), \
+    (test_path, test_class) = split_dataset(file_path_list, seed=1337)
 
-# # split the data into unlabeled and labeled data
-# print('spliting the dataset')
-# unlabeled_path, labeled_path, \
-#     _, labeled_class = train_test_split(
-#         path,
-#         classification,
-#         test_size=0.1,
-#         random_state=0,
-#     )
+# generate panda dataframes and export it to csv
+print('generating unlabeled dataframe')
+unlabeled_data = pd.DataFrame(
+    data={
+        'email': [preprocess.read_email(path) for path in unlabeled_path],
+        'class': [UNLABEL for _ in range(len(unlabeled_path))]
+    },
+    columns=['email', 'class'],
+)
 
-# # split data into train and test data
-# train_path, test_path, \
-#     train_class, test_class = train_test_split(
-#         labeled_path,
-#         labeled_class,
-#         test_size=0.2,
-#         random_state=0,
-#     )
+print('generating train dataframe')
+train_data = pd.DataFrame(
+    data={
+        'email': [preprocess.read_email(path) for path in train_path],
+        'class': [SPAM if cl == 'spam' else HAM for cl in train_class]
+    },
+    columns=['email', 'class'],
+)
 
-# # generate panda dataframes and export it to csv
-# print('generating unlabeled dataframe')
-# unlabeled_data = pd.DataFrame(
-#     data={
-#         'email': [preprocess.read_email(path) for path in unlabeled_path],
-#         'class': [UNLABEL for _ in range(len(unlabeled_path))]
-#     },
-#     columns=['email', 'class'],
-# )
+print('generating test dataframe')
+test_data = pd.DataFrame(
+    data={
+        'email': [preprocess.read_email(path) for path in test_path],
+        'class': [SPAM if cl == 'spam' else HAM for cl in test_class]
+    },
+    columns=['email', 'class'],
+)
 
-# print('generating train dataframe')
-# train_data = pd.DataFrame(
-#     data={
-#         'email': [preprocess.read_email(path) for path in train_path],
-#         'class': [SPAM if cl == 'spam' else HAM for cl in train_class]
-#     },
-#     columns=['email', 'class'],
-# )
-
-# print('generating test dataframe')
-# test_data = pd.DataFrame(
-#     data={
-#         'email': [preprocess.read_email(path) for path in test_path],
-#         'class': [SPAM if cl == 'spam' else HAM for cl in test_class]
-#     },
-#     columns=['email', 'class'],
-# )
-
-# # export dataframes to csv
-# unlabeled_data.to_csv('unlabeled_data.csv')
-# train_data.to_csv('train_data.csv')
-# test_data.to_csv('test_data.csv')
+# export dataframes to csv
+unlabeled_data.to_csv('unlabeled_data.csv')
+train_data.to_csv('train_data.csv')
+test_data.to_csv('test_data.csv')
 
 # read_csv files
 unlabeled_data = pd.read_csv('unlabeled_data.csv', encoding='iso-8859-1')
--- spam/common/utils.py
+++ spam/common/utils.py
@@ -3,6 +3,8 @@
 
 import os
 
+from sklearn.cross_validation import train_test_split
+
 
 def get_file_path_list(dataset_meta):
     """
@@ -25,3 +27,34 @@ def get_file_path_list(dataset_meta):
             ))
 
     return email_file_path_list
+
+
+def split_dataset(file_path_list, seed=0):
+    """
+    A helper function that accepts list of file paths
+    and splits them into unlabeled, train, test sets.
+    """
+    # transform list of tuple into two list
+    # e.g. [('/path/to/file', 'spam')] ==> ['path/to/file'], ['spam']
+    path, classification = zip(*file_path_list)
+
+    # split the data into labeled and unlabeled
+    unlabeled_path, labeled_path, \
+        _, labeled_class = train_test_split(
+            path,
+            classification,
+            test_size=0.1,
+            random_state=seed,
+        )
+
+    # split data into train and test data
+    train_path, test_path, \
+        train_class, test_class = train_test_split(
+            labeled_path,
+            labeled_class,
+            test_size=0.2,
+            random_state=seed,
+        )
+
+    return unlabeled_path, (train_path, train_class), \
+            (test_path, test_class)
