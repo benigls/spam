872c9947eac7bf366cc96d679701027df8f1ef65
Ben <iglesia.ben@gmail.com>
2015-12-26 08:27:23 +0000

    Add deeplearning module to main module.

--- spam.py
+++ spam.py
@@ -9,6 +9,7 @@ import pandas as pd
 from spam.common import DATASET_META
 from spam.common.utils import get_file_path_list, split_dataset
 from spam.preprocess import preprocess
+from spam.deeplearning import StackedDenoisingAutoEncoder
 
 
 UNLABEL = -1
@@ -16,10 +17,12 @@ HAM = 0
 SPAM = 1
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-r', '--read', action='store_true')
+parser.add_argument('-c', '--csv', action='store_true')
+parser.add_argument('-n', '--npz', action='store_true')
+parser.add_argument('-m', '--model', action='store_true')
 args = parser.parse_args()
 
-if not args.read:
+if args.csv:
     file_path_list = get_file_path_list(DATASET_META)
 
     print('Spliting the dataset..')
@@ -58,7 +61,8 @@ if not args.read:
     unlabeled_data.to_csv('unlabeled_data.csv')
     train_data.to_csv('train_data.csv')
     test_data.to_csv('test_data.csv')
-else:
+
+if args.npz:
     print('Reading csv files..')
     unlabeled_data = pd.read_csv('data/csv/unlabeled_data.csv',
                                  encoding='iso-8859-1')
@@ -67,15 +71,49 @@ else:
     test_data = pd.read_csv('data/csv/test_data.csv',
                             encoding='iso-8859-1')
 
-print('Generating feature vectors..')
-unlabeled_feat, _ = preprocess.count_vectorizer([unlabeled_data['email']])
-train_feat, test_feat = preprocess.count_vectorizer([
-    train_data['email'], test_data['email']
-])
+    print('Generating feature vectors..')
+    unlabeled_feat, _ = preprocess.count_vectorizer([unlabeled_data['email']])
+    train_feat, test_feat = preprocess.count_vectorizer([
+        train_data['email'], test_data['email']
+    ])
 
-print('Exporting npz files inside data/npz/ ..')
-np.savez('data/npz/unlabeled_feature', X=unlabeled_feat)
-np.savez('data/npz/train_feature',
-         X=train_feat, Y=train_data['class'].values)
-np.savez('data/npz/test_feature',
-         X=test_feat, Y=test_data['class'].values)
+    print('Exporting npz files inside data/npz/ ..')
+    np.savez('data/npz/unlabeled_feature', X=unlabeled_feat)
+    np.savez('data/npz/train_feature',
+             X=train_feat, Y=train_data['class'].values)
+    np.savez('data/npz/test_feature',
+             X=test_feat, Y=test_data['class'].values)
+
+if args.model:
+    print('Building model..')
+    sda = StackedDenoisingAutoEncoder(
+        batch_size=128, classes=2, epochs=0, n_folds=4,
+        hidden_layers=[2500, 1700, 1000, 300, ],
+        noise_layers=[0.3, 0.2, 0.1, ],
+    )
+    model = sda.build_sda()
+
+    model.add(sda.build_finetune())
+    X_train, Y_train = sda.dataset['train_data']
+    X_test, Y_test = sda.dataset['train_data']
+
+    print('Finetuning the model..')
+    model.fit(
+        X_train, Y_train, batch_size=sda.batch_size,
+        nb_epoch=sda.epochs, show_accuracy=True,
+        validation_data=(X_test, Y_test), validation_split=0.1,
+    )
+
+    print('Evaluating model..')
+    score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
+
+    print('Test score: {}'.format(score[0]))
+    print('Test accuracy: {}'.format(score[1]))
+
+    print('Saving model structure and weights..')
+    open('spam_ms_epochs_{}.json'.format(sda.epochs), 'w') \
+        .write(model.to_json())
+    model.save_weights('spam_mw_epochs_{}.hdf5'
+                       .format(sda.epochs), overwrite=True)
+
+    print('Done!')
