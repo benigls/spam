88cbaa3353a4dc6599400628e754478a677cb3a3
Ben <iglesia.ben@gmail.com>
2016-01-13 06:47:27 +0000

    Update main url.

--- django_spam/django_spam/urls.py
+++ django_spam/django_spam/urls.py
@@ -5,5 +5,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'classifier/', include('classifier.urls', namespace='classifier')),
+    url(r'^classifier/', include('classifier.urls', namespace='classifier')),
 ]
