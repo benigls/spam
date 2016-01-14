992f171488b17ef7ddbc3370a348fb332ce75a32
Ben <iglesia.ben@gmail.com>
2016-01-14 03:33:59 +0000

    Add classify url.

--- django_spam/classifier/urls.py
+++ django_spam/classifier/urls.py
@@ -2,9 +2,10 @@
 
 from django.conf.urls import url
 
-from .views import HomePageView
+from .views import HomePageView, classify
 
 
 urlpatterns = [
     url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'classify/$', classify, name='classify'),
 ]
