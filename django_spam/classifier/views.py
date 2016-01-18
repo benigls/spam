af751991672910a507731002b8e8c857a519d5d5
Ben <iglesia.ben@gmail.com>
2016-01-18 05:03:48 +0000

    Add classifier model.

--- django_spam/classifier/views.py
+++ django_spam/classifier/views.py
@@ -11,15 +11,25 @@ from nltk import tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
-from keras.model import model_from_config
+from keras.models import model_from_config
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
 
-PREFIX = '../experiments/100_exp'
-MODEL = model_from_config('{}/model_structure.json'.format(PREFIX))
-MODEL.load_weights('{}/model_weights.hdf5'.forma(PREFIX))
+PREFIX = '../experiments/exp_103'
 
-with open('{}/vocabulary.json', 'r') as f:
+with open('{}/model_structure.json'.format(PREFIX), 'r') as f:
+    MODEL_STRUCTURE = json.load(f)
+
+with open('{}/vocabulary.json'.format(PREFIX), 'r') as f:
     VOCABULARY = json.load(f)
+    VOCABULARY = ' '.join(VOCABULARY)
+
+MODEL = model_from_config(MODEL_STRUCTURE)
+MODEL.load_weights('{}/model_weights.hdf5'.format(PREFIX))
+
+TOKENIZER = Tokenizer(nb_words=1000)
+TOKENIZER.fit_on_texts(VOCABULARY)
 
 
 def clean_text(text):
@@ -45,6 +55,18 @@ def classify(request):
     if request.method == 'POST':
         body = request.POST.get('body')
         body = clean_text(body)
+
+        X = TOKENIZER.texts_to_sequences([body, ])
+        X = pad_sequences(X, maxlen=800, dtype='float64')
+
+        y = MODEL.predict_classes(X)[0]
+        Y = 'SPAM' if y else 'WEW'
+        response_data = {'label': Y}
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type='application/json'
+        )
 
     else:
         return HttpResponse(
